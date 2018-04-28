cf34904f371bde80a125de8e2429373844d536e6
Furetur Phyarell <furetur@gmail.com>
2018-04-28 23:41:06 +0500

    header ready

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require('gulp');
 
 const sass = require('gulp-sass');
+const browserSync = require('browser-sync').create();
+
+gulp.task('copy-images', () => {
+  return gulp.src('./src/img/*')
+    .pipe(gulp.dest('./dist/img'));
+});
+
+gulp.task('copy-images:watch', () => {
+  return gulp.watch('./src/img/*', ['copy-images']);
+});
 
 gulp.task('html', () => {
   return gulp.src('./src/**/*.html')
@@ -14,13 +24,19 @@ gulp.task('html:watch', () => {
 gulp.task('sass', () => {
   return gulp.src('./src/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('sass:watch', () => {
   return gulp.watch('./src/sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['html', 'sass']);
+gulp.task('default', ['html', 'sass', 'copy-images']);
 
-gulp.task('watch', ['html:watch', 'sass:watch'])
+gulp.task('serve', ['default', 'html:watch', 'sass:watch', 'copy-images:watch'], () => {
+  browserSync.init({
+    server: 'dist/',
+  });
+  gulp.watch('src/**/*.html').on('change', browserSync.reload);
+})
