58c17186c53ccb5f6039bed2386697b3f885298f
furetur <furetur@gmail.com>
2018-05-19 20:24:37 +0500

    img folder structure + images are now resized

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 
+const gm = require('gulp-gm');
+
 gulp.task('copy-images', () => {
   return gulp.src('./src/img/*')
     .pipe(gulp.dest('./dist/img'));
@@ -39,4 +41,36 @@ gulp.task('serve', ['default', 'html:watch', 'sass:watch', 'copy-images:watch'],
     server: 'dist/',
   });
   gulp.watch('src/**/*').on('change', browserSync.reload);
-})
+});
+
+// images -----------------------------
+const es = require('event-stream');
+
+const IMG_SRC_PATH = './src/img';
+const IMG_DEST_PATH = './dist/img/';
+
+
+function resizeImages(folder, width) {
+  return gulp.src(`${IMG_SRC_PATH}/${folder}/${width}px/*`)
+    .pipe(gm(gmfile => gmfile.resize(width)))
+    .pipe(gulp.dest(IMG_DEST_PATH));
+}
+
+
+function copyImages(folder) {
+  return gulp.src(`${IMG_SRC_PATH}/${folder}/*`)
+    .pipe(gulp.dest(IMG_DEST_PATH));
+}
+
+
+gulp.task('images', () => {
+  return es.merge([
+    resizeImages('blocks', 48),
+    resizeImages('blocks', 150),
+    resizeImages('structures', 96),
+    resizeImages('structures', 960),
+    resizeImages('structures', 1920),
+    copyImages('env'),
+    copyImages('misc'),
+  ]);
+});
